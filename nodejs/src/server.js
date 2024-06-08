19a6c7a60829205556dd21ee98220e1e57dcae8a
manh0912hehe <125677565+manh0912hehe@users.noreply.github.com>
2024-06-08 12:08:15 +0700

    nodejs commit

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -20,7 +20,7 @@ initWebRoutes(app);
 
 connectDB();
 
-let port = process.env.PORT || 8080;  //Port === undefined => Port = 6060
+let port = process.env.PORT || 8082;  //Port === undefined => Port = 6060
 
 app.listen(port, () => {
     //callback
